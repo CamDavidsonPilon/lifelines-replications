d74f70bf17a8bbff5ac5cab153fbd2ef0a467743
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-06-15 13:20:14 -0400

    lint

--- replications/Crowther_Royston_Clements_2020.py
+++ replications/Crowther_Royston_Clements_2020.py
@@ -15,6 +15,7 @@ class CRCSplineFitter(SplineFitterMixin, ParametricRegressionFitter):
     """
     Crowther MJ, Royston P, Clements M. A flexible parametric accelerated failure time model.
     """
+
     _scipy_fit_method = "SLSQP"
 
     def __init__(self, n_baseline_knots, *args, **kwargs):
@@ -25,7 +26,11 @@ class CRCSplineFitter(SplineFitterMixin, ParametricRegressionFitter):
     def _create_initial_point(self, Ts, E, entries, weights, Xs):
         return [
             {
-                **{"beta_": np.zeros(len(Xs.mappings["beta_"])), "gamma0_": np.array([0.0]), "gamma1_": np.array([0.1])},
+                **{
+                    "beta_": np.zeros(len(Xs.mappings["beta_"])),
+                    "gamma0_": np.array([0.0]),
+                    "gamma1_": np.array([0.1]),
+                },
                 **{"gamma%d_" % i: np.array([0.0]) for i in range(2, self.n_baseline_knots)},
             }
         ]
@@ -40,10 +45,13 @@ class CRCSplineFitter(SplineFitterMixin, ParametricRegressionFitter):
         Xbeta = np.dot(Xs["beta_"], params["beta_"])
         logT = np.log(T)
 
-        H = safe_exp(params['gamma0_'] + params["gamma1_"] * (logT + Xbeta))
+        H = safe_exp(params["gamma0_"] + params["gamma1_"] * (logT + Xbeta))
 
         for i in range(2, self.n_baseline_knots):
-            H *= safe_exp(params["gamma%d_" % i] * self.basis(logT + Xbeta, self.knots[i-1], min_knot=self.knots[0], max_knot=self.knots[-1]))
+            H *= safe_exp(
+                params["gamma%d_" % i]
+                * self.basis(logT + Xbeta, self.knots[i - 1], min_knot=self.knots[0], max_knot=self.knots[-1])
+            )
         return H
 
 
@@ -51,27 +59,26 @@ class CRCSplineFitter(SplineFitterMixin, ParametricRegressionFitter):
 def generate_data(n=20000):
     X = np.random.binomial(1, 0.5, size=n)
     Z = np.random.normal(0, 4, size=n)
-    T_actual = np.random.exponential(1/np.exp(-5 + 1 * X + 1 * Z))
+    T_actual = np.random.exponential(1 / np.exp(-5 + 1 * X + 1 * Z))
     C = 10 * np.random.random(size=n)
 
     T_observed = np.minimum(T_actual, C)
     E = T_actual < C
-    return pd.DataFrame({'X': X, 'E': E, 'T': T_observed, 'Z': Z, "constant": 1})
+    return pd.DataFrame({"X": X, "E": E, "T": T_observed, "Z": Z, "constant": 1})
 
 
 df = generate_data()
 
 
 regressors = {
-    "beta_": ["X", "Z" ],
+    "beta_": ["X", "Z"],
     "gamma0_": ["constant"],
     "gamma1_": ["constant"],
     "gamma2_": ["constant"],
 }
 
-cf = CRCSplineFitter(3).fit(df, 'T', 'E', regressors=regressors)
+cf = CRCSplineFitter(3).fit(df, "T", "E", regressors=regressors)
 cf.print_summary()
-
 
 
 # section 4.2
@@ -89,7 +96,9 @@ def generate_data(n=1000):
     # confirm that S(5).mean() == 0.106 ✅
     # h(t) looks like graph ✅
     def S(t, x):
-        return p*np.exp(-lambda1 * (t ** gamma1) * np.exp(-x * beta * gamma1)) + (1-p) * np.exp(-lambda2 * (t ** gamma2) * np.exp(-x * beta * gamma2))
+        return p * np.exp(-lambda1 * (t ** gamma1) * np.exp(-x * beta * gamma1)) + (1 - p) * np.exp(
+            -lambda2 * (t ** gamma2) * np.exp(-x * beta * gamma2)
+        )
 
     T_actual = np.empty(n)
 
@@ -103,7 +112,7 @@ def generate_data(n=1000):
     MAX_TIME = 5
     T_observed = np.minimum(MAX_TIME, T_actual)
     E = T_actual < MAX_TIME
-    return pd.DataFrame({"E": E,"T": T_observed, "X": X, "constant": 1})
+    return pd.DataFrame({"E": E, "T": T_observed, "X": X, "constant": 1})
 
 
 df = generate_data()
@@ -116,11 +125,7 @@ regressors = {
     "gamma3_": ["constant"],
 }
 
-WeibullAFTFitter().fit(df.drop('constant', axis=1), 'T', 'E').print_summary()
-cf = CRCSplineFitter(4).fit(df, 'T', 'E', regressors=regressors)
+WeibullAFTFitter().fit(df.drop("constant", axis=1), "T", "E").print_summary()
+cf = CRCSplineFitter(4).fit(df, "T", "E", regressors=regressors)
 cf.print_summary()
-cf.predict_hazard(df)[[0,1,2,3]].plot()
-
-
-
-
+cf.predict_hazard(df)[[0, 1, 2, 3]].plot()
